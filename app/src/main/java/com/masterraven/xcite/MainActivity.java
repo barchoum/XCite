b95c4690f9a8d3912bccdbc73db0b8afe4dad9c4
Tahir <tahirferli@gmail.com>
2018-08-20 12:44:51 -0400

    Main Activity.

--- app/src/main/java/com/masterraven/xcite/MainActivity.java
+++ app/src/main/java/com/masterraven/xcite/MainActivity.java
@@ -1,17 +1,13 @@
 package com.masterraven.xcite;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-        finish();
+        setContentView(R.layout.activity_main);
     }
 }
