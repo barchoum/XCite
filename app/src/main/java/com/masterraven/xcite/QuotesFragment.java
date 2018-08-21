c132809baf86bc41eca13866fcb363ed344e95a6
Tahir <tahirferli@gmail.com>
2018-08-21 13:12:24 -0400

    Add fragments and layouts Start responsive design Add Butterknife

---
+++ app/src/main/java/com/masterraven/xcite/QuotesFragment.java
@@ -0,0 +1,26 @@
+package com.masterraven.xcite;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Tahir on 21.8.2018.
+ */
+
+public class QuotesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.frag_quotes, container, false);
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+}
